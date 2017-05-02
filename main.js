a90a272ad194c838fcb7860a6c5fe2828dfa90f2
Ronan Viana <ronanssviana@gmail.com>
2017-05-02 14:02:50 -0300

    Testing header menu with PhantomJS + CasperJS

---
+++ main.js
@@ -0,0 +1,65 @@
+//Header menu redirect
+
+var casper = require('casper').create();
+var links;
+
+function getLinks() {
+  var links = document.querySelectorAll('ul.head-menu li a');
+  return Array.prototype.map.call(links, function (e) {
+      return e.getAttribute('href')
+  });
+}
+
+casper.start('http://www.endlessos.com/pt-br');
+
+casper.then(function () {
+    links = this.evaluate(getLinks);
+});
+
+casper.run(function () {
+    for(var i in links) {
+        console.log(links[i]);
+    }
+    casper.done();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
